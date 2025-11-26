0266a251f9759d9122c61ba8d24c57ab50669302
kview <kasreder@naver.com>
2025-11-27 03:40:13 +0900

    ..

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -14,11 +14,11 @@ class Database {
 
   private constructor() {
     this.dbConfig = {
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST || '127.0.0.1',
       port: parseInt(process.env.DB_PORT || '3306'),
       user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'oamanage',
+      password: process.env.DB_PASSWORD || 'kasreder',
+      database: process.env.DB_NAME || 'oa_asset_manager',
     };
 
     this.pool = mysql.createPool({
