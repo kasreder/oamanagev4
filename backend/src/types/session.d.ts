fbd8af69ee96bf2bf34c726fd5ffa8e5461379a7
kasreder <kasreder@naver.com>
2025-12-07 22:24:55 +0900

    feat: 사용자 로그인 정보와 점수 필드 추가

--- backend/src/types/session.d.ts
+++ backend/src/types/session.d.ts
@@ -4,10 +4,12 @@ declare module 'express-session' {
   interface SessionData {
     user?: {
       id: number;
+      loginMethod: string;
       kakaoId: string;
       nickname: string;
       email?: string;
       profileImage?: string;
+      score: number;
       role?: string;
     };
   }
